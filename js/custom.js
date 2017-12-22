67902b33bce961395d5a5fda26e01708f53d6a07
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-12-22 17:48:18 -0600

    testimonials set up with carousel.

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,57 @@
 -                   SERVICES SECTION
 =======================================================*/
 $(function () {
-    // animate on scroll
+    // trigger animation on scroll
     new WOW().init();
 });
+
+/*=======================================================
+-                   WORK SECTION
+=======================================================*/
+$(function() {
+    // magnific-popup image modal
+    $("#work").magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery:{
+            enabled:true
+        }
+    });
+});
+
+/*=======================================================
+-                   TEAM SECTION
+=======================================================*/
+$(function() {
+    // image carousel
+    $("#team-members").owlCarousel({
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        nav: true,
+        responsive: {
+            600:{
+                items:3
+            }
+        }
+    });
+});
+
+/*=======================================================
+-                 TESTIMONIALS SECTION
+=======================================================*/
+$(function() {
+    // image carousel
+    $("#customers-testimonials").owlCarousel({
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        nav: true,
+        responsive: {
+            0:{
+                items:1
+            }
+        }
+    });
+});
+
